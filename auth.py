da2780543a7be7bb1575ffdcf239a7647228bf57
DMGochoa <diego.moreno@utp.edu.co>
2023-03-03 12:01:10 -0500

    Last changes until the push

--- auth.py
+++ auth.py
@@ -22,14 +22,7 @@ def authenticate_user(credentials):
     generic authentication function
     returns True if user is correct and False otherwise
     '''
-    #
-    # BACKEND CONNECTION 
-    #cred = {
-    #    "username" : "DMoreno",
-    #    "password" : "Di4724",
-    #    "code" : 52327171
-    #}
-
+    
     logger.debug('Start the POST to the API')
     response = requests.post('http://127.0.0.1:9000/login', 
                              json=credentials)
